7c2ca32b36b7d3e6c1999c70d3c8189d50dce0b3
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-12-13 23:19:10 +0900

    fix

---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,19 @@
+import MDXComponents from '@theme-original/MDXComponents';
+
+import { TwitterTweetEmbed } from 'react-twitter-embed'
+
+import AdventCalendar from '@site/src/components/AdventCalendar';
+import CaptionImage from '@site/src/components/CaptionImage';
+import Details from '@site/src/components/Details';
+import Flex from '@site/src/components/Flex';
+import Video from '@site/src/components/Video';
+
+export default {
+  ...MDXComponents,
+  TwitterTweetEmbed,
+  AdventCalendar,
+  CaptionImage,
+  Details,
+  Flex,
+  Video,
+};
